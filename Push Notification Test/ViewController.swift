f100a72c802388b6078a9cb2764fcc053d60b63c
lfernandezaragon <lucasfernandezaragon@gmail.com>
2018-10-24 12:27:35 +0200

    Initial Commit

---
+++ Push Notification Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Push Notification Test
+//
+//  Created by lucas fernández on 24/10/2018.
+//  Copyright © 2018 lucas fernández. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Push Notification TestTests/Push_Notification_TestTests.swift
@@ -0,0 +1,34 @@
+//
+//  Push_Notification_TestTests.swift
+//  Push Notification TestTests
+//
+//  Created by lucas fernández on 24/10/2018.
+//  Copyright © 2018 lucas fernández. All rights reserved.
+//
+
+import XCTest
+@testable import Push_Notification_Test
+
+class Push_Notification_TestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
